b32db3ed1f50b9ce13ae82f56cdc21bd7a774a1b
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-04-30 23:39:28 +0530

    Add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { Outlet } from "react-router-dom";
+import {  useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [collection, setCollection] = useState(null);
+  const [cart, setCart] = useState(() => {
+    const response = localStorage.getItem("CART");
+    const local = JSON.parse(response);
+    if (local !== null) {
+      return local;
+    } else {
+      return [];
+    }
+  });
+  const [amount, setAmount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid gap-0 min-h-svh">
+        <Header cart = {cart} />
+        <Outlet
+          context={{
+            collectionData: [collection, setCollection],
+            cartData: [cart, setCart],
+            amountData: [amount, setAmount]
+          }}
+        />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
